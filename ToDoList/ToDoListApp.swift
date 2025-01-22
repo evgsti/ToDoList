79e779ea328aadde9b11194cf70cb3e4629a7599
Evgeniy <dev.shipitsyn@yandex.ru>
2025-01-22 12:09:16 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Евгений on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
